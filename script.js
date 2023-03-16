5e43e7e34256d267f6746011894c09fa9364ae58
ZenKhalil <feattzen@gmail.com>
2023-03-16 11:18:21 +0100

    Spørgsmål 1

--- script.js
+++ script.js
@@ -3,42 +3,58 @@
 window.addEventListener("load", start);
 
 function start() {
-    console.log("Unit Converter 23 is running");
-    document.querySelector("#conversiontype").addEventListener("change", conversionChange);
-    document.querySelector("#inputvalue").addEventListener("keyup", inputChange);
+  console.log("Unit Converter 23 is running");
+  document
+    .querySelector("#conversiontype")
+    .addEventListener("change", conversionChange);
+  document.querySelector("#inputvalue").addEventListener("keyup", inputChange);
 }
 
 function conversionChange() {
-    selectConversion(this.value);
+  selectConversion(this.value);
 }
 
 function selectConversion(conversion) {
-    console.log(`Selected conversion: ${conversion}`);
-    setConversionType(conversion);
-    displayInputUnit(conversion);
-    displayOutputUnit(conversion);
-    performConversion();
+  console.log(`Selected conversion: ${conversion}`);
+  setConversionType(conversion);
+  displayInputUnit(conversion);
+  displayOutputUnit(conversion);
+  performConversion();
 }
 
 function inputChange() {
-    if(this.value) {
-        // only accept valid inputs
-        selectInput(this.value);
-    }
+  if (this.value) {
+    // only accept valid inputs
+    selectInput(this.value);
+  }
 }
 
 function selectInput(inputvalue) {
-    console.log(`input value: ${inputValue}`);
-    setInputValue(inputValue);
-    performConversion()
+  console.log(`input value: ${inputValue}`);
+  setInputValue(inputValue);
+  performConversion();
 }
 
 function performConversion() {
-    const type = getConversionType();
-    const value = getInputValue();
+  const type = getConversionType();
+  const value = getInputValue();
 
-    const result = convert(type,value);
-    displayOutputValue(result);
+  const result = convert(type, value);
+  displayOutputValue(result);
 }
 
 /* ********************************* */
+
+// (Svar på spørgsmål 1)
+let conversionType;
+
+function setConversionType(conversion) {
+  conversionType = conversion;
+  return conversionType;
+}
+
+function getConversionType() {
+  return conversionType;
+}
+
+// (Slut på svar)
