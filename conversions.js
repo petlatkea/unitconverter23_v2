4a4e0177f5e85f5452573fccb1aaf08c3d1c3de7
Jaes98 <marcus.jaes@gmail.com>
2023-03-16 11:02:31 +0100

    update conversions fToC cToF

---
+++ conversions.js
@@ -0,0 +1,10 @@
+function fToC(fahrenheit) {
+  const fht = ((fahrenheit - 32) * 5) / 9;
+  console.log(fht);
+  return fht;
+}
+function cToF(celcius) {
+  const cel = (celcius * 9 / 5)+32;
+  console.log(cel);
+  return cel;
+}
