bd48d81172a3e72e27f416e50cdfdf384f8460fb
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-03-16 11:15:14 +0100

    hihi

---
+++ conversions.js
@@ -0,0 +1,15 @@
+"use-strict";
+
+function feetToM(feet) {
+  console.log("feetToM");
+  const meters = feet / 3.2808;
+  return meters;
+}
+
+function meToFeet(meters) {
+  console.log("mToFeet");
+  const feet = meters * 3.2808;
+  return feet;
+}
+
+// gruppe: Hamsa, Lise, Yousra;
